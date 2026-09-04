b145900483d71a391b082bed70ed58ca8a9dd9d8
FRANCISCO JAVIER RESTREPO ROLDAN <franciscorestrepo1151558@correo.itm.edu.co>
2026-09-04 15:08:36 -0500

    Add changes

---
+++ exercise34/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace exercise34
+{
+    public class HuorglassisEmpy
+    {
+        static void Main(string[] args)
+        {
+            int N = 11;
+            int[,] matrix = new int[N, N];
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (j >= i && j <= N - i - 1 || j <= i && j >= N - i - 1)
+                    {
+                        matrix[i, j] = 2*i+j;
+                    }
+                    else {
+                    matrix[i,j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Complete matrix:");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Hourglass");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (matrix[i,j] ==0)
+
+                        Console.Write( "\t");
+                    else
+                        Console.Write(matrix[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ logica aplicada/backened/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace backened
+{
+
+
+    class exercise31
+    {
+        static void Main()
+        {
+
+
+            int N = 5;
+            int[,] matrix = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = i + j+10;
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (j <= i)
+                        Console.Write(matrix[i, j] + "\t");
+                    else
+                        Console.Write("\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+
+
+
+    }
+}
+
