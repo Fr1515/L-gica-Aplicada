af606f46aaa67fce2b409d1b8967b9222190808d
FRANCISCO JAVIER RESTREPO ROLDAN <franciscorestrepo1151558@correo.itm.edu.co>
2026-09-07 16:54:40 -0500

    add changes

---
+++ Ejercicio 31/Program.cs
@@ -0,0 +1,45 @@
+﻿int N = 5;
+int[,] matriz = new int[N, N];
+
+Console.WriteLine("Ingrese el orden de la matriz:10");
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        matriz[i, j] = i + j + 10;
+    }
+}
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(matriz[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (j <= i)
+            Console.Write(matriz[i, j] + "\t");
+        else
+            Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ejercicio 34/Program.cs
@@ -0,0 +1,42 @@
+﻿int N = 11;
+int[,] matrix = new int[N, N];
+
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (j >= i && j <= N - i - 1 || j <= i && j >= N - i - 1)
+        {
+            matrix[i, j] = 2 * i + j;
+        }
+        else
+        {
+            matrix[i, j] = 0;
+        }
+    }
+}
+Console.WriteLine("Mtriz completa");
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Reloj de Arena");
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (matrix[i, j] == 0)
+
+            Console.Write("\t");
+        else
+            Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
